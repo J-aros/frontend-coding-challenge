b9f44c818ae456ec5666b34aec25ca5e93a23498
J-aros <j.aros.cortes@gmail.com>
2022-04-08 02:01:21 -0400

    fix: fix routes of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
-import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Sidebar from "./components/Layout/Sidebar/Sidebar";
 import Header from "./components/Layout/Header/Header";
 import Container from "./components/Layout/Container/Container";
 import Footer from "./components/Layout/Footer/Footer";
-import "./index.css";
+import Bitcoin from "./components/Pages/Bitcoin";
+import DolarAcuerdo from "./components/Pages/DolarAcuerdo";
+import DolarObservado from "./components/Pages/DolarObservado";
+import Euro from "./components/Pages/Euro";
+import Imacec from "./components/Pages/Imacec";
+import LibraDeCobre from "./components/Pages/LibraDeCobre";
+import TasaDeDesempleo from "./components/Pages/TasaDeDesempleo";
+import UnidadFomento from "./components/Pages/UnidadFomento";
+import UnidadTributariaMensual from "./components/Pages/UnidadTributariaMensual";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div>
-      <Container>
-        <Header />
-        <Sidebar />
-        <Footer />
-      </Container>
-    </div>
+    <Container>
+      <Sidebar />
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Bitcoin />} />
+          <Route path="/dolaracuerdo" element={<DolarAcuerdo />} />
+          <Route path="/dolarobservado" element={<DolarObservado />} />
+          <Route path="/euro" element={<Euro />} />
+          <Route path="/imacec" element={<Imacec />} />
+          <Route path="/libradecobre" element={<LibraDeCobre />} />
+          <Route path="/tasadesempleo" element={<TasaDeDesempleo />} />
+          <Route path="/unidadfomento" element={<UnidadFomento />} />
+          <Route path="/utm" element={<UnidadTributariaMensual />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </Container>
   );
 }
 
