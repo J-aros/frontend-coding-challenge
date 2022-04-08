8b528a5382575ecb8a778f670f6570240df10464
J-aros <j.aros.cortes@gmail.com>
2022-04-08 03:14:52 -0400

    refactor: html code is passed to component SidebarLinks

--- src/components/Layout/Sidebar/Sidebar.jsx
+++ src/components/Layout/Sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
-import { NavLink } from "react-router-dom";
 import classes from "./Sidebar.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCode } from "@fortawesome/free-solid-svg-icons";
+import SidebarLinks from "./SidebarLinks";
 
 const Sidebar = () => {
   return (
@@ -10,20 +10,7 @@ const Sidebar = () => {
         <FontAwesomeIcon className={classes.sidenav__icon} icon={faCode} />
         BeFlow
       </h2>
-
-      <ul className={classes.sidenav__list}>
-        <li className={classes.sidenav__list_item}>Libra de Cobre</li>
-        <li className={classes.sidenav__list_item}>Tasa de Desempleo</li>
-        <li className={classes.sidenav__list_item}>IMACEC</li>
-        <li className={classes.sidenav__list_item}>Euro / CLP</li>
-        <li className={classes.sidenav__list_item}>Dólar observado</li>
-        <li className={classes.sidenav__list_item}>Dólar acuerdo</li>
-        <li className={classes.sidenav__list_item}>Unidad de Fomento</li>
-        <li className={classes.sidenav__list_item}>
-          Unidad Tributaria Mensual
-        </li>
-        <li className={classes.sidenav__list_item}>Bitcoin</li>
-      </ul>
+      <SidebarLinks />
     </aside>
   );
 };
