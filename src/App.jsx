aeef463162c7fcc8cd1ae2f4332327990d6a7c89
J-aros <j.aros.cortes@gmail.com>
2022-04-08 01:34:11 -0400

    refactor: import components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
-import Sidebar from "./components/Sidebar/Sidebar";
+import Sidebar from "./components/Layout/Sidebar/Sidebar";
+import Header from "./components/Layout/Header/Header";
+import Container from "./components/Layout/Container/Container";
+import Footer from "./components/Layout/Footer/Footer";
+import "./index.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <Sidebar />
+    <div>
+      <Container>
+        <Header />
+        <Sidebar />
+        <Footer />
+      </Container>
     </div>
   );
 }
